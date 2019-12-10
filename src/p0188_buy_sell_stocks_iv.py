0021ff5b1e6804df6c82ed110ff6ddb40c666ec5
Mirzad Mohandespour <12888868+mirzadm@users.noreply.github.com>
2019-12-10 10:04:18 -0600

    Problem 188: dynamic programming, first iteration

--- src/p0188_buy_sell_stocks_iv.py
+++ src/p0188_buy_sell_stocks_iv.py
@@ -3,6 +3,54 @@
 from typing import List, Tuple
 
 
+def max_profit_dynamic_prog(max_transactions: int, prices: List[int]) -> int:
+    """Calculates max profit in buying and selling stocks.
+
+    Uses dynamic programming.
+
+    Restrictions:
+        1- Number of transactions <= `max_transactions`
+        1- No more than one transaction per day
+        2- Must sell before buying new stocks
+
+    Args:
+        `price`: List of daily stock prices.
+    Returns:
+        Max profit in buying and selling stocks.
+    """
+    price_intervals = parse_intervals(prices)
+    if len(price_intervals) == 0 or max_transactions == 0:
+        return 0
+    if len(price_intervals) < max_transactions:
+        max_transactions = len(price_intervals)
+    return max_profit_dp_helper(max_transactions, price_intervals)
+
+
+def max_profit_dp_helper(
+    max_transactions: int, price_intervals: List[Tuple[int]]
+) -> int:
+    """Helper to calculate max profit in buying and selling stocks."""
+    curr_profits = {}
+    next_profits = {}
+    k = 0
+    while k < max_transactions:
+        for left in range(len(price_intervals)):
+            max_profit = 0
+            for right in range(left, len(price_intervals)):
+                if price_intervals[right][1] > price_intervals[left][0]:
+                    profit = price_intervals[right][1] - price_intervals[left][0]
+                    if k > 1:
+                        if right + 1 in curr_profit:
+                            profit += curr_profit[right + 1]
+                    if profit > max_profit:
+                        max_profit = profit
+            next_profits[left] = max_profit
+        curr_profits = next_profits
+        next_profits = {}
+        k += 1
+    return max(curr_profits.values())
+
+
 def max_profit(max_transactions: int, prices: List[int]) -> int:
     """Calculates max profit in buying and selling `max_transactions` stocks.
 
--- tests/test_p0188.py
+++ tests/test_p0188.py
@@ -1,9 +1,27 @@
 import unittest
 
-from src.p0188_buy_sell_stocks_iv import max_profit
+from src.p0188_buy_sell_stocks_iv import (
+    max_profit, max_profit_dynamic_prog as max_profit_dp
+)
 
 
 class TestCases(unittest.TestCase):
+    def text_max_profit_dp(self):
+        self.assertEqual(max_profit_dp(1, []), 0)
+        self.assertEqual(max_profit_dp(0, [1, 2]), 0)
+        self.assertEqual(max_profit_dp(1,[1]), 0)
+        self.assertEqual(max_profit_dp(2, [1]), 0)
+        self.assertEqual(max_profit_dp(1, [1, 2]), 1)
+        self.assertEqual(max_profit_dp(2, [1, 2]), 1)
+        self.assertEqual(max_profit_dp(1, [2, 1]), 0)
+        self.assertEqual(max_profit_dp(2, [2, 1]), 0)
+        self.assertEqual(max_profit_dp(1, [1, 2, 1, 5]), 4)
+        self.assertEqual(max_profit_dp(2, [1, 2, 1, 5]), 5)
+        self.assertEqual(max_profit_dp(1, [1, 3, 2, 5]), 4)
+        self.assertEqual(max_profit_dp(2, [1, 3, 2, 5]), 5)
+        self.assertEqual(max_profit_dp(1, [1, 3, 2, 5]), 4)
+        self.assertEqual(max_profit_dp(2, [1, 3, 2, 5]), 5)
+
     def text_max_profit(self):
         self.assertEqual(max_profit(1, []), 0)
         self.assertEqual(max_profit(0, [1, 2]), 0)
