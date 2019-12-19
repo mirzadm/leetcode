c3f2566566bafcd4f6f5332bc58b14f2e35b2820
Mirzad Mohandespour <12888868+mirzadm@users.noreply.github.com>
2019-12-19 16:01:34 -0600

    Problem 15: clean up, add docstrings, update tests

--- src/p0015_sum_of_three.py
+++ src/p0015_sum_of_three.py
@@ -4,56 +4,28 @@ from collections import defaultdict
 from typing import List
 
 
-def sum_of_three_brute_force(nums: List[int]) -> List[List[int]]:
-    triplets = []
-    if len(nums) < 3:
-        return []
-    for i in range(len(nums) - 2):
-        for j in range(i + 1, len(nums) - 1):
-            for k in range(j + 1, len(nums)):
-                if nums[i] + nums[j] + nums[k] == 0:
-                    sorted_triplet = sorted([nums[i], nums[j], nums[k]])
-                    if sorted_triplet not in triplets:
-                        triplets.append(sorted_triplet)
-    return triplets
+def sum_of_three_n2_partition(nums: List[int]) -> List[List[int]]:
+    """Finds all unique triplets in `nums` where sum(triplet) is zero.
 
+    An O(n^2) solution that relies to partitioning numbers into positive and
+    negative sublists. It therefore improves time complexity for cases where
+    number of positive numbers is considerably larger than negative numbers or
+    vice versa.
 
-def sum_of_three_n2(nums):
-    if len(nums) < 3:
-        return []
-    sum_to_pairs = defaultdict(set)
-    counts = defaultdict(int)
-    for num in nums:
-        counts[num] += 1
-    for i in range(len(nums) - 1):
-        for j in range(i + 1, len(nums)):
-            t = tuple(sorted((nums[i], nums[j])))
-            sum_to_pairs[nums[i] + nums[j]].add(t)
-    triplets = set()
-    for num in nums:
-        if -num in sum_to_pairs:
-            for pair in sum_to_pairs[-num]:
-                if (
-                    num not in pair
-                    or (num in pair and num != 0 and counts[num] > 1)
-                    or (num in pair and num == 0 and counts[0] >= 3)
-                ):
-                    triplet = tuple(sorted((num, *pair)))
-                    triplets.add(triplet)
-    triplets_list = [list(t) for t in triplets]
-    return triplets_list
-
-
-def sum_of_three_n2_partition(nums):
+    Args:
+        `nums`: List of integers.
+    Returns:
+        List of triples which are 3-element lists.
+    """
     if len(nums) < 3:
         return []
     counts = defaultdict(int)
     positives = []
     negatives = []
     for num in nums:
-        if num >=0:
+        if num > 0:
             positives.append(num)
-        else:
+        elif num < 0:
             negatives.append(num)
         counts[num] += 1
     triplets = set()
@@ -61,13 +33,17 @@ def sum_of_three_n2_partition(nums):
         for pos in positives:
             lookup_value = -(neg + pos)
             if lookup_value in counts:
+                # Account for potential repeated values
+                # For instance given: lookup_value = -(-2 + 4) = -2
+                # Make sure there are at least two -2's in input numbers.
                 if (
                     lookup_value not in (neg, pos)
                     or lookup_value == neg and counts[neg] >= 2
                     or lookup_value == pos and counts[pos] >= 2
                 ):
                     triplet = tuple(sorted((lookup_value, neg, pos)))
-                    triplets.add(triplet)
+                    triplets.add(triplet) # Only unique triplets
+    # Handle the case for (0, 0, 0) triplet separately
     if 0 in counts and counts[0] >= 3:
         triplets.add((0, 0, 0))
     triplets_list = [list(t) for t in triplets]
--- tests/test_p0015.py
+++ tests/test_p0015.py
@@ -1,32 +1,22 @@
 import unittest
 from src.p0015_sum_of_three import (
-    sum_of_three_brute_force as sum_n3,
-    sum_of_three_n2 as sum_n2,
     sum_of_three_n2_partition as sum_n2_partition,
 )
 
 
 class TestCases(unittest.TestCase):
-    def test_sum_n3(self):
-        self.assertEqual(sum_n3([]), [])
-        self.assertEqual(sum_n3([1]), [])
-        self.assertEqual(sum_n3([1, 2]), [])
-        self.assertEqual(sum_n3([0, 0, 0]), [[0, 0, 0]])
-        self.assertEqual(sum_n3([-1, -1, 2]), [[-1, -1, 2]])
-        self.assertEqual(sum_n3([-1, -1, 2, 1, 0, -1]), [[-1, -1, 2], [-1, 0, 1]])
-
-    def test_sum_n2(self):
-        self.assertEqual(sum_n2([]), [])
-        self.assertEqual(sum_n2([1]), [])
-        self.assertEqual(sum_n2([1, 2]), [])
-        self.assertEqual(sum_n2([0, 0, 0]), [[0, 0, 0]])
-        self.assertEqual(sum_n2([-1, -1, 2]), [[-1, -1, 2]])
-        self.assertEqual(sum_n2([-1, -1, 2, 1, 0, -1]), [[-1, -1, 2], [-1, 0, 1]])
-
     def test_sum_n2_partition(self):
         self.assertEqual(sum_n2_partition([]), [])
         self.assertEqual(sum_n2_partition([1]), [])
         self.assertEqual(sum_n2_partition([1, 2]), [])
         self.assertEqual(sum_n2_partition([0, 0, 0]), [[0, 0, 0]])
+        self.assertEqual(sum_n2_partition([0, 0, 1]), [])
+        self.assertEqual(sum_n2_partition([-1, 0, 1]), [[-1, 0, 1]])
+        self.assertEqual(sum_n2_partition([-1, 1, 2]), [])
         self.assertEqual(sum_n2_partition([-1, -1, 2]), [[-1, -1, 2]])
-        self.assertEqual(sum_n2_partition([-1, -1, 2, 1, 0, -1]), [[-1, -1, 2], [-1, 0, 1]])
+        self.assertEqual(sum_n2_partition(
+            [-1, -1, 2, -1, -1, 2]), [[-1, -1, 2]]
+        )
+        self.assertEqual(sum_n2_partition(
+            [-1, -1, 2, 1, 0, -1]), [[-1, -1, 2], [-1, 0, 1]]
+        )
