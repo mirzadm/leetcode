cfbe60981cbd86dc4216903620ccf18aa40cff52
Mirzad Mohandespour <12888868+mirzadm@users.noreply.github.com>
2020-01-30 13:50:46 -0600

    Problem 416: Refactor, update docstrings and tests

--- src/p0416_partition_equal_subset_sum.py
+++ src/p0416_partition_equal_subset_sum.py
@@ -1,18 +1,19 @@
 """Problem 416. Partition Equal Subset Sum"""
 
-from typing import List
+from typing import List, Dict, Tuple
 
 
 def partition_equal_subset_sum(nums: List[int]) -> bool:
-    """Checks if array can be partitioned into two subsets of equal sum.
+    """Check if `nums` can be partitioned to 2 subsets of equal sum.
 
-    Each of the array elements will not exceed 100.
-    The array size will not exceed 200.
+    Wraps `can_partition`, initializes `cache`, and takes care of edge cases.
+    Each of the array elements will not exceed 100. The array size will not
+    exceed 200.
 
     Args:
         `nums`: List of positive integers.
     Returns:
-        True if there is a way to partition the list otherwise false.
+        True if there is a way to partition the list otherwise False.
     """
     if not nums:
         return True
@@ -23,15 +24,30 @@ def partition_equal_subset_sum(nums: List[int]) -> bool:
     return can_partition(nums, len(nums) - 1, nums_sum / 2, cache)
 
 
-def can_partition(nums, last_index, sum_value, cache):
-    if sum_value == 0:
+def can_partition(
+    nums: List[int], length: int, total: int, cache: Dict[Tuple[int, int], bool]
+) -> bool:
+    """Is there a subset of `nums[:length]` whose sum equals `total`?
+
+    Uses memoization to dynamically store intemediate results in `cache`.
+
+    Args:
+        `nums`: List of integers.
+        `length`: Length of `nums` to consider.
+        `total`: Target sum value.
+        `cache`: Dictionary that stores intermediate boolean results.
+    Returns:
+        True is a subset was found otherwise False.
+    """
+    if total == 0:
         return True
-    if last_index < 0 or sum_value < 0:
+    if length == 0 or total < 0:
         return False
-    if (last_index, sum_value) not in cache:
-        cache[(last_index, sum_value)] = (
-            can_partition(nums, last_index - 1, sum_value - nums[last_index], cache
+    if (length, total) not in cache:
+        cache[(length, total)] = (
+            can_partition(
+                nums, length - 1, total - nums[length - 1], cache
             )
-            or can_partition(nums, last_index - 1, sum_value, cache)
+            or can_partition(nums, length - 1, total, cache)
         )
-    return cache[(last_index, sum_value)]
+    return cache[(length, total)]
--- tests/test_p0416.py
+++ tests/test_p0416.py
@@ -6,7 +6,9 @@ class TestCases(unittest.TestCase):
     def test_partition_equal_subset_sum(self):
         self.assertTrue(func([]))
         self.assertFalse(func([1]))
+        self.assertFalse(func([2]))
         self.assertTrue(func([1, 1]))
+        self.assertFalse(func([1, 2]))
         self.assertTrue(func([1, 2, 1]))
         self.assertFalse(func([1, 3, 2, 1]))
         self.assertFalse(func([1, 2, 3, 5]))
