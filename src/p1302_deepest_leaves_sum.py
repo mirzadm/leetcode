3e5fbfb2058db734846a3c7fda541c531f65b28a
Mirzad Mohandespour <12888868+mirzadm@users.noreply.github.com>
2020-03-24 23:58:30 -0500

    Problem 1302

---
+++ src/p1302_deepest_leaves_sum.py
@@ -0,0 +1,54 @@
+"""Given a binary tree, return the sum of values of its deepest leaves."""
+
+import math
+from collections import deque
+from typing import List
+
+
+class TreeNode:
+    def __init__(self, value):
+        self.val = value
+        self.left = None
+        self.right = None
+
+
+def deepest_leaves_sum(root: TreeNode) -> int:
+    if not root:
+        return None
+    nodes = tree_to_full_list(root)
+    deepest_level_count = 2 ** math.floor(math.log2(len(nodes)))
+    result = sum(
+        node.val for node in nodes[-deepest_level_count:] if node is not None
+    )
+    return result
+
+
+def tree_to_full_list(root: TreeNode) -> List[TreeNode]:
+    if not root:
+        return []
+    q = deque()
+    q.append(root)
+    result = []
+    deeper_level = True
+    level_length = 1
+    while deeper_level:
+        level = []
+        for _ in range(level_length):
+            level.append(q.popleft())
+        if any(level):
+            result.extend(level)
+            for node in level:
+                if node is None:
+                    q.extend([None, None])
+                else:
+                    q.extend([node.left, node.right])
+        else:
+            deeper_level = False
+    return result
+
+
+if __name__ == "__main__":
+    n = TreeNode(1)
+    n.left = TreeNode(2)
+    n.right = TreeNode(3)
+    print(deepest_leaves_sum(n))
