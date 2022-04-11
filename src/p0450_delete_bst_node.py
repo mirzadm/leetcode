48944c70ac81128f765a5ed722fbad2457177df4
Mirzad Mohandespour <12888868+mirzadm@users.noreply.github.com>
2022-04-10 21:32:31 -0500

    Problem 450: Add docstrings

--- src/p0450_delete_bst_node.py
+++ src/p0450_delete_bst_node.py
@@ -1,6 +1,7 @@
 """Problem 450. Delete a node from a BST."""
 
-from typing import Tuple, Union
+from collections import deque
+from typing import Tuple, Union, List
 
 class TreeNode:
     def __init__(self, key: int) -> None:
@@ -12,9 +13,34 @@ class BinSearchTree:
     def __init__(self, root):
         self.root = root
 
-    def find_node_and_parent(self, key: int) -> Tuple[TreeNode, TreeNode]:
-        current = self.root
+    def _convert_binsearch_list_to_tree(keys: List[int]):
+        """Converst a binary search-formatted list to its corresponding binary search tree."""
+        if not keys:
+            return None
+        root = TreeNode(keys[0])
+        nodes = deque()
+        nodes.appendleft(root)
+        for i, key in enumerate(keys):
+            left_child_index = 2 * i + 1
+            right_child_index = 2 * i + 2
+            if left_child_index >= len(keys):
+                break
+            else:
+                parent = nodes.pop()
+                left_child = TreeNode(keys[left_child_index])
+                parent.left = left_child
+                nodes.appendleft(left_child)
+                if right_child_index < len(keys):
+                    right_child = TreeNode(keys[right_child_index])
+                    parent.right = right_child
+                    nodes.appendleft(right_child)
+        del deque
+        return root
+
+    def _find_node_and_parent(self, key: int) -> Tuple[Union[TreeNode, None], Union[TreeNode, None]]:
+        """Return node containing key and its parent node."""
         parent = None
+        current = self.root
         while current:
             if current.key == key:
                 return parent, current
@@ -24,7 +50,8 @@ class BinSearchTree:
                 parent, current = current, current.right
         return None, None
 
-    def find_min(self, root: TreeNode) -> Tuple[Union[None, TreeNode], Union[None, TreeNode]]:
+    def _find_min(self, root: TreeNode) -> Tuple[Union[None, TreeNode], Union[None, TreeNode]]:
+        """Return node with min key in subtree at root and its parent node."""
         current = root
         parent = None
         while current:
@@ -34,7 +61,8 @@ class BinSearchTree:
                 return parent, current
         return None, None
 
-    def find_max(self, root: TreeNode) -> Tuple[Union[None, TreeNode], Union[None, TreeNode]]:
+    def _find_max(self, root: TreeNode) -> Tuple[Union[None, TreeNode], Union[None, TreeNode]]:
+        """Return node with max key in subtree at root and its parent node."""
         current = root
         parent = None
         while current:
@@ -44,7 +72,7 @@ class BinSearchTree:
                 return parent, current
         return None, None
 
-    def remove_subtree_root(self, root: TreeNode) -> Union[TreeNode, None]:
+    def _remove_subtree_root(self, root: TreeNode) -> Union[TreeNode, None]:
         """Given the root of a BST, remove the root and return a new BST root
 
         It does not "delete" the current root.
@@ -54,7 +82,7 @@ class BinSearchTree:
 
         new_root = None
         if root.left:
-            parent, new_root = self.find_max(root.left)
+            parent, new_root = self._find_max(root.left)
             if parent is None:
                 new_root.right = root.right
             else:
@@ -63,7 +91,7 @@ class BinSearchTree:
                 parent.right = new_root.left
                 new_root.left, new_root.right = root.left, root.right
         elif root.right:
-            parent, new_root = self.find_min(root.right)
+            parent, new_root = self._find_min(root.right)
             if parent is None:
                 new_root.left = root.left
             else:
@@ -75,10 +103,10 @@ class BinSearchTree:
 
     def delete_key(self, key: int) -> None:
         """Deletes a key from the BST"""
-        parent, node = self.find_node_and_parent(key)
+        parent, node = self._find_node_and_parent(key)
         if node is None:
             return
-        new_subtree_root = self.delete_subtree_root(node)
+        new_subtree_root = self._remove_subtree_root(node)
         if node == self.root:
             self.root = new_subtree_root
         elif parent.left == node:
