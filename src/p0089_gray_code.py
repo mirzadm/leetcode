9cd0738319a0f2a8959b14b0044abd35fbbc2520
Mirzad Mohandespour <12888868+mirzadm@users.noreply.github.com>
2022-04-15 11:06:36 -0500

    Problem 89: Improve the performance
    - Use one int instead of list of ints to represent a code
    - Use xor to invert bits
    - Break the loop as soon as one bit changes

--- src/p0089_gray_code.py
+++ src/p0089_gray_code.py
@@ -3,18 +3,6 @@
 from typing import List
 
 
-def _convert_to_int(bits: List[int]) -> int:
-    """Convert a list of 0/1 to unsigned int, left being less significant"""
-    if bits == []:
-        raise ValueError("bits must not be empty")
-    power = 1
-    num = 0
-    for b in bits:
-        num += b * power
-        power <<= 1
-    return num
-
-
 def _powers_of_two(n: int) -> List[int]:
     """Return a list of powers of 2 from 0 to n"""
     if n < 0:
@@ -29,17 +17,19 @@ def gray_code_sequence_generator(n: int) -> List[int]:
     """Return a circular list of n-bit gray codes starting at 0."""
     if n < 1:
         raise ValueError("n must be >= 1")
-    gray_code_nums = []
+    gray_code_sequence = []
     powers_of_two = _powers_of_two(n)
-    gray_code = [0] * n
-    gray_code_nums.append(0)
+    gray_code = 0
+    gray_code_sequence.append(0)
     for i in range(1, powers_of_two[n]):
         for j in range(n):
-            # The following condition determines when to switch a certain bit
+            # The following condition determines when to invert a certain bit
+            # For instance bit 1 changes every 4 bits starting at code number 2
             if i % powers_of_two[j + 1] == powers_of_two[j]:
-                gray_code[j] = 1 - gray_code[j]
-        gray_code_nums.append(_convert_to_int(gray_code))
-    return gray_code_nums
+                gray_code ^= powers_of_two[j]
+                break # Only one bit changes in each iteration
+        gray_code_sequence.append(gray_code)
+    return gray_code_sequence
 
 
 if __name__ == "__main__":
--- tests/test_p0089.py
+++ tests/test_p0089.py
@@ -2,21 +2,10 @@
 
 import pytest
 from src.p0089_gray_code import (
-    _convert_to_int,
     _powers_of_two,
     gray_code_sequence_generator as gray_code,
 )
 
-def test_convert_to_int():
-    with pytest.raises(ValueError):
-        _convert_to_int([])
-    assert _convert_to_int([0]) == 0
-    assert _convert_to_int([0, 0]) == 0
-    assert _convert_to_int([1]) == 1
-    assert _convert_to_int([1, 0]) == 1
-    assert _convert_to_int([0, 1]) == 2
-    assert _convert_to_int([1, 1]) == 3
-    assert _convert_to_int([0, 0, 1]) == 4
 
 def test_powers_of_two():
     with pytest.raises(ValueError):
