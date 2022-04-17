caca8f2cf8469cbb6f22e2346dfcf0347b194de2
Mirzad Mohandespour <12888868+mirzadm@users.noreply.github.com>
2022-04-17 14:33:10 -0500

    Problem 89: Add the simple mathematical solution
    - Add the simple math solution
    - Add tests

--- src/p0089_gray_code.py
+++ src/p0089_gray_code.py
@@ -2,6 +2,10 @@
 
 from typing import List
 
+###############################################################################
+# A solution based on the observation that each bit change follows a certain
+# pattern.
+###############################################################################
 
 def _powers_of_two(n: int) -> List[int]:
     """Return a list of powers of 2 from 0 to n"""
@@ -23,14 +27,22 @@ def gray_code_sequence_generator(n: int) -> List[int]:
     gray_code_sequence.append(0)
     for i in range(1, powers_of_two[n]):
         for j in range(n):
-            # The following condition determines when to invert a certain bit
-            # For instance bit 1 changes every 4 bits starting at code number 2
+            # The condition determines when to invert a certain bit
+            # For instance, bit 1 changes every 4 bits starting at code number 2
             if i % powers_of_two[j + 1] == powers_of_two[j]:
                 gray_code ^= powers_of_two[j]
                 break # Only one bit changes in each iteration
         gray_code_sequence.append(gray_code)
     return gray_code_sequence
 
+###############################################################################
+# A solution using the simple mathematical formula of n ^ (n >> 1)
+###############################################################################
 
-if __name__ == "__main__":
-    print(gray_code_sequence_generator(4))
+def gray_code_formula(n: int) -> List[int]:
+    if n < 1:
+        raise ValueError("n must be >= 1")
+    gray_codes = []
+    for i in range(2 ** n):
+        gray_codes.append(i ^ (i >> 1))
+    return gray_codes
--- tests/test_p0089.py
+++ tests/test_p0089.py
@@ -4,6 +4,7 @@ import pytest
 from src.p0089_gray_code import (
     _powers_of_two,
     gray_code_sequence_generator as gray_code,
+    gray_code_formula,
 )
 
 
@@ -18,6 +19,13 @@ def test_powers_of_two():
 def test_gray_code():
     with pytest.raises(ValueError):
         gray_code(0)
+    assert gray_code(1) == [0, 1]
+    assert gray_code(2) == [0, 1, 3, 2]
+    assert gray_code(3) == [0, 1, 3, 2, 6, 7, 5, 4]
+
+def test_gray_code_formula():
+    with pytest.raises(ValueError):
+        gray_code_formula(0)
     assert gray_code(1) == [0, 1]
     assert gray_code(2) == [0, 1, 3, 2]
     assert gray_code(3) == [0, 1, 3, 2, 6, 7, 5, 4]
