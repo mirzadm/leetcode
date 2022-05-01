100c93468ccac0d0607488a73b0c247e630dd04c
Mirzad Mohandespour <12888868+mirzadm@users.noreply.github.com>
2022-05-01 14:14:49 -0500

    Problem 1029: Refactor and use pytest

--- src/p1029_two_city_scheduling.py
+++ src/p1029_two_city_scheduling.py
@@ -7,16 +7,16 @@ def two_city_scheduling(costs: List[List[int]]) -> int:
     """Calculates the min cost for two city scheduling problem.
 
     Args:
-        `costs`: List of 2-element lists representing costs for each city for
-        for each candidate. Must includes even number of cost pairs.
+        `costs`: List of cost pairs representing costs for flying a
+                 candidate to a each of two cities.
+                 Assumes an even number of items in the list.
     Returns:
-        Min cost to assign candidates to cities.
+        Min cost to fly candidates to one of the two cities.
     """
+    middle = len(costs) // 2
     sorted_costs = sorted(costs, key=lambda item: item[0] - item[1])
-    min_cost = 0
-    for i in range(len(costs)):
-        if i < len(costs) / 2:
-            min_cost += sorted_costs[i][0]
-        else:
-            min_cost += sorted_costs[i][1]
+    min_cost = (
+        sum(cost_pair[0] for cost_pair in sorted_costs[: middle])
+        + sum(cost_pair[1] for cost_pair in sorted_costs[middle:])
+    )
     return min_cost
--- tests/test_p1029.py
+++ tests/test_p1029.py
@@ -1,10 +1,9 @@
-import unittest
+"""Unit tests for problem 1029."""
 from src.p1029_two_city_scheduling import two_city_scheduling
 
 
-class TestCases(unittest.TestCase):
-    def test_two_city_scheduling(self):
-        self.assertEqual(two_city_scheduling([[1, 10], [20, 2]]), 3)
-        self.assertEqual(two_city_scheduling([[20, 2], [1, 10]]), 3)
-        self.assertEqual(two_city_scheduling([[1, 10], [2, 20]]), 12)
-        self.assertEqual(two_city_scheduling([[10, 1], [20, 2]]), 12)
+def test_two_city_scheduling():
+    assert two_city_scheduling([[1, 10], [20, 2]]) == 3
+    assert two_city_scheduling([[20, 2], [1, 10]]) == 3
+    assert two_city_scheduling([[1, 10], [2, 20]]) == 12
+    assert two_city_scheduling([[10, 1], [20, 2]]) == 12
