21770c49941524b39ae430219a0bce5e0f183f2b
Mirzad Mohandespour <12888868+mirzadm@users.noreply.github.com>
2022-05-29 23:44:13 -0500

    Problem 910: Refactor and use pytest

--- src/p0910_smallest_range_2.py
+++ src/p0910_smallest_range_2.py
@@ -1,15 +1,28 @@
 """Problem 910: Smallest range II."""
 
-def smallest_range_2(nums, k):
-    origin_min = min(nums)
-    min_value = origin_min + k
-    max_value = origin_min + k
+from typing import List
+
+
+def smallest_range_2(nums: List[int], k: int) -> int:
+    """
+    Find smallest possible range when each number in the nums can be
+    replaces by either number + k or number - k.
+
+    Solution assumes an optimal min-max so far and updates them with each new number.
+    """
+    nums_min = min(nums)
+    current_min = nums_min + k
+    current_max = nums_min + k
     for num in nums:
-        if num + k > max_value and num - k < min_value:
-            if num + k - max_value > min_value - (num - k):
-                min_value = num - k
-            else:
-                max_value = num + k
-        elif num + k > max_value and num - k > max_value:
-            max_value = num - k
-    return max_value - min_value
+        # Only a num that extends the range is to be reckon with!
+        if num + k > current_max:
+            if num - k < current_min:
+                # num extends the range
+                # Minimize how much the range is extended by num
+                if num + k - current_max > current_min - (num - k):
+                    current_min = num - k
+                else:
+                    current_max = num + k
+            elif num - k > current_max:
+                current_max = num - k
+    return current_max - current_min
--- tests/test_p0910.py
+++ tests/test_p0910.py
@@ -1,21 +1,20 @@
-import unittest
 from src.p0910_smallest_range_2 import smallest_range_2
 
 
-class TestCases(unittest.TestCase):
-    def test_smallest_range_2(self):
-        self.assertEqual(smallest_range_2([1], 0), 0)
-        self.assertEqual(smallest_range_2([1], 1), 0)
-        self.assertEqual(smallest_range_2([1], 5), 0)
+def test_smallest_range_2():
+    assert smallest_range_2([1], 0) == 0
+    assert smallest_range_2([1], 1) == 0
+    assert smallest_range_2([1], 5) == 0
 
-        self.assertEqual(smallest_range_2([1, 2], 0), 1)
-        self.assertEqual(smallest_range_2([1, 2], 1), 1)
-        self.assertEqual(smallest_range_2([1, 2], 5), 1)
+    assert smallest_range_2([1, 2], 0) == 1
+    assert smallest_range_2([1, 2], 1) == 1
+    assert smallest_range_2([1, 2], 5) == 1
 
-        self.assertEqual(smallest_range_2([2, 1], 0), 1)
-        self.assertEqual(smallest_range_2([2, 1], 1), 1)
-        self.assertEqual(smallest_range_2([2, 1], 5), 1)
+    assert smallest_range_2([2, 1], 0) == 1
+    assert smallest_range_2([2, 1], 1) == 1
+    assert smallest_range_2([2, 1], 5) == 1
 
-        self.assertEqual(smallest_range_2([1, 3, 6], 0), 5)
-        self.assertEqual(smallest_range_2([1, 3, 6], 1), 3)
-        self.assertEqual(smallest_range_2([1, 3, 6], 3), 3)
+    assert smallest_range_2([1, 3, 6], 0) == 5
+    assert smallest_range_2([1, 3, 6], 1) == 3
+    assert smallest_range_2([1, 3, 6], 3) == 3
+    assert smallest_range_2([1, 3, 6], 10) == 5
