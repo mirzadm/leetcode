1620d1b84116b4a54cc6e6376891047d16a4f7b1
Mirzad Mohandespour <12888868+mirzadm@users.noreply.github.com>
2023-07-26 15:14:26 -0500

    Prolem 1883: update unit tests

--- tests/test_p1883.py
+++ tests/test_p1883.py
@@ -3,13 +3,19 @@ from src.p1883_min_skips import min_skips
 
 def test_min_skips():
     # Impossible case
-    assert min_skips(dist=[3], speed=2, hoursBefore=1) == -1
-    assert min_skips(dist=[3, 2], speed=2, hoursBefore=2) == -1
+    assert min_skips(dist=[3], speed=2, hours_before=1) == -1
+    assert min_skips(dist=[3, 2], speed=2, hours_before=2) == -1
     # Possible
     # No skips
-    assert min_skips(dist=[3], speed=2, hoursBefore=2) == 0
-    assert min_skips(dist=[3, 3], speed=2, hoursBefore=4) == 0
+    assert min_skips(dist=[3], speed=2, hours_before=2) == 0
+    assert min_skips(dist=[3, 3], speed=2, hours_before=4) == 0
     # 1 skip
-    assert min_skips(dist=[3, 3], speed=2, hoursBefore=3) == 1
-    # 2 skips
-    assert min_skips(dist=[3, 3, 2], speed=2, hoursBefore=4) == 2
+    assert min_skips(dist=[3, 3], speed=2, hours_before=3) == 1
+    # 1 skip
+    assert min_skips(dist=[3, 3, 2], speed=2, hours_before=4) == 1
+    # More complex example
+    assert min_skips(dist=[.3, .2, .7, 0, .8, .1, .6], speed=1, hours_before=6) == 0
+    assert min_skips(dist=[.3, .2, .7, 0, .8, .1, .6], speed=1, hours_before=5) == 1
+    assert min_skips(dist=[.3, .2, .7, 0, .8, .1, .6], speed=1, hours_before=4) == 2
+    assert min_skips(dist=[.3, .2, .7, 0, .8, .1, .6], speed=1, hours_before=3) == 5
+
