705cb7f3407a1609a17daa6e43b57b7ae1ce8184
Mirzad Mohandespour <12888868+mirzadm@users.noreply.github.com>
2023-09-06 15:37:16 -0500

    Problem 955: fix bugs

--- src/p0955.py
+++ src/p0955.py
@@ -11,27 +11,28 @@ from typing import List
 
 
 def min_deletion_size(words: List[str]) -> int:
-    word_length = len(words[0])
     current_index = 0
     deleted_chars = 0
-    while current_index < word_length:
+    while current_index < len(words[0]):
         any_equal_to = False
         any_greater_than = False
         for i in range(len(words) - 1):
             if words[i][:current_index + 1] > words[i + 1][:current_index + 1]:
-                # Delete this character
-                deleted_chars += 1
                 any_greater_than = True
-                words = _delete_char(words, current_index)
                 break
-        if not any_greater_than:
+        if any_greater_than:
+            deleted_chars += 1
+            words = _delete_char(words, current_index)
+        else:
             for i in range(len(words) - 1):
                 if words[i][:current_index + 1] == words[i + 1][:current_index + 1]:
                     any_equal_to = True
                     break
-            if not any_equal_to:
+            if any_equal_to:
+                current_index += 1
+            else:
                 return deleted_chars
-        current_index += 1
+
     return deleted_chars
 
 
